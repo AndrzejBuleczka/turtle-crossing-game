288b1f8f86dda21e49f5bb49e1766158d441a7dd
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2024-05-28 20:05:22 +0200

    feat: add player class definition and methods

--- main.py
+++ main.py
@@ -8,7 +8,29 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+scoreboard = Scoreboard()
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager = CarManager()
+    car_manager.create_car()
+    car_manager.move_car()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    if player.ycor() > 280:
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
+
+screen.exitonclick()
