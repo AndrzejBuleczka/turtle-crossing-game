22a08a379053c633cc09dcb72a0ad20bb9b981be
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2024-05-29 11:42:48 +0200

    refactor: change car manager methods

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
 scoreboard = Scoreboard()
 screen.listen()
 screen.onkey(player.go_up, "Up")
@@ -18,9 +19,8 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
 
-    car_manager = CarManager()
     car_manager.create_car()
-    car_manager.move_car()
+    car_manager.move_cars()
 
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
