096064989db9919d0901a43adc1854e0771fbe39
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2024-05-29 12:10:45 +0200

    fix: fix car manager cars bugs

--- car_manager.py
+++ car_manager.py
@@ -6,10 +6,9 @@ STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager(Turtle):
+class CarManager:
 
     def __init__(self):
-        super().__init__()
         self.all_cars = []
         self.car_speed = STARTING_MOVE_DISTANCE
 
@@ -19,15 +18,10 @@ class CarManager(Turtle):
             new_car = Turtle("square")
             new_car.shapesize(stretch_wid=1, stretch_len=2)
             new_car.penup()
-            new_car.color(COLORS[random.randint(0, 5)])
-            new_car.setheading(180)
-            new_car.goto(300, random.randint(-250, 250))
+            new_car.color(random.choice(COLORS))
+            random_y = random.randint(-250, 250)
+            new_car.goto(300, random_y)
             self.all_cars.append(new_car)
-
-    # def reset(self):
-    #     for car in self.all_cars:
-    #         car.goto(300, random.randint(-250, 250))
-
 
     def move_cars(self):
         for car in self.all_cars:
